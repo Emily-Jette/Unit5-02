20c6d7321d68b5b47d09481b31132f8ad3c238fd
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-16 16:52:51 +0000

    changed spacing and deleted useless #text

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //  this code will run two alerts after button is clicked.
 document.getElementById('button').addEventListener('click', message)
 
-function message() {
-  alert ('Hey, thanks for coming!')
-  alert ('Going so soon? Oh well, have a great day!')
+function message () {
+  alert('Hey, thanks for coming!')
+  alert('Going so soon? Oh well, have a great day!')
 }
 
 // This code changes the text on a button. It was a test.
 document.getElementById('button2').addEventListener('click', text)
 
-function text() {
+function text () {
   document.getElementById('button2').innerHTML = 'Oh Hey!'
 }
 
 // this code will change the text color of the paragraph
 document.getElementById('button3').addEventListener('click', color)
 
-function color() {
+function color () {
   document.getElementById('text').style.color = 'lightseagreen'
-  document.getElementById('text').style.fontFamily='fantasy'
-  document.getElementById('text').style.fontSize='30px'
+  document.getElementById('text').style.fontFamily = 'fantasy'
+  document.getElementById('text').style.fontSize = '30px'
 }
