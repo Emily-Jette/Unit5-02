10865b9a0ef09e681261bc3272c7ccc30915305b
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-16 15:06:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('button').addEventListener('click', message)
+
+function message(){
+  alert ('Hey, thanks for coming!')
+  alert ('Going so soon? Oh well, have a great day!')
+}
